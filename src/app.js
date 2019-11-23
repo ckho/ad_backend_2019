df2fd3fc827ee7cf6f07dac7f2e6dfdb6676ffc2
ckho <me@ckho.info>
2019-11-23 16:51:05 +0800

    Update CORS - Action Verb

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use(function(req, res, next){
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
